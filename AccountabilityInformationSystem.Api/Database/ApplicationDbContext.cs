c5934e6add0957bbda564b3e747ded15cd770900
kiryazovkosta <kosta.kiryazov@gmail.com>
2025-11-10 20:30:21 +0200

    Add MeasurementPointData

--- AccountabilityInformationSystem.Api/Database/ApplicationDbContext.cs
+++ AccountabilityInformationSystem.Api/Database/ApplicationDbContext.cs
@@ -1,24 +1,52 @@
 ﻿using AccountabilityInformationSystem.Api.Common.Constants;
 using AccountabilityInformationSystem.Api.Entities;
+using AccountabilityInformationSystem.Api.Entities.Excise;
 using AccountabilityInformationSystem.Api.Entities.Flow;
 using AccountabilityInformationSystem.Api.Entities.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace AccountabilityInformationSystem.Api.Database;
 
 public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
     : DbContext(options)
 {
+    public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<Product> Products { get; set; }
+
     public DbSet<Warehouse> Warehouses { get; set; }
     public DbSet<Ikunk> Ikunks { get; set; }
     public DbSet<MeasurementPoint> MeasurementPoints { get; set; }
+    public DbSet<MeasurementPointData> MeasurementPointsData { get; set; }
+
+    public DbSet<ApCode> ApCodes { get; set; }
+    public DbSet<ApCode> BrandNames { get; set; }
+    public DbSet<ApCode> CnCodes { get; set; }
+
     public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(SchemasConstants.Application);
 
+        ApplyDecimalDefaultPrecision(modelBuilder);
+
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+
+    private static void ApplyDecimalDefaultPrecision(ModelBuilder modelBuilder)
+    {
+        foreach (IMutableEntityType entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (IMutableProperty property in entityType.GetProperties())
+            {
+                if (property.ClrType == typeof(decimal) || property.ClrType == typeof(decimal?))
+                {
+                    property.SetPrecision(18);
+                    property.SetScale(3);
+                }
+            }
+        }
     }
 }
 
---
+++ AccountabilityInformationSystem.Api/Database/Configurations/Flow/MeasurementPointDataConfiguration.cs
@@ -0,0 +1,121 @@
+using AccountabilityInformationSystem.Api.Common.Constants;
+using AccountabilityInformationSystem.Api.Entities.Flow;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AccountabilityInformationSystem.Api.Database.Configurations.Flow;
+
+public sealed class MeasurementPointDataConfiguration : IEntityTypeConfiguration<MeasurementPointData>
+{
+    public void Configure(EntityTypeBuilder<MeasurementPointData> builder)
+    {
+        builder.ToTable("MeasurementPointData", "flow");
+
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Id)
+            .IsRequired()
+            .HasMaxLength(EntitiesConstants.IdMaxLength);
+
+        builder.Property(e => e.MeasurementPointId)
+            .IsRequired()
+            .HasMaxLength(EntitiesConstants.IdMaxLength);
+
+        builder.Property(e => e.Number)
+            .IsRequired()
+            .HasMaxLength(EntitiesConstants.IdMaxLength);
+
+        builder.Property(e => e.BeginTime)
+            .IsRequired();
+
+        builder.Property(e => e.EndTime)
+            .IsRequired();
+
+        builder.Property(e => e.FlowDirectionType)
+            .IsRequired()
+            .HasMaxLength(EntitiesConstants.EnumMaxLength)
+            .HasConversion(
+                val => val.ToString(),
+                val => Enum.Parse<FlowDirectionType>(val));
+
+        builder.Property(e => e.ProductId)
+            .IsRequired()
+            .HasMaxLength(EntitiesConstants.IdMaxLength);
+
+        builder.Property(e => e.TotalizerBeginGrossObservableVolume)
+            .IsRequired(false);
+
+        builder.Property(e => e.TotalizerEndGrossObservableVolume)
+            .IsRequired(false);
+
+        builder.Property(e => e.TotalizerBeginGrossStandardVolume)
+            .IsRequired(false);
+
+        builder.Property(e => e.TotalizerEndGrossStandardVolume)
+            .IsRequired(false);
+
+        builder.Property(e => e.TotalizerBeginMass)
+            .IsRequired(false);
+
+        builder.Property(e => e.TotalizerEndMass)
+            .IsRequired(false);
+
+        builder.Property(e => e.GrossObservableVolume)
+            .IsRequired(false);
+
+        builder.Property(e => e.GrossStandardVolume)
+            .IsRequired(false);
+
+        builder.Property(e => e.Mass)
+            .IsRequired(false);
+
+        builder.Property(e => e.AverageObservableDensity)
+            .IsRequired(false);
+
+        builder.Property(e => e.AverageReferenceDensity)
+            .IsRequired(false);
+
+        builder.Property(e => e.AverageTemperature)
+            .IsRequired(false);
+
+        builder.Property(e => e.AlcoholContent)
+            .IsRequired(false);
+
+        builder.Property(e => e.BatchNumber)
+            .HasMaxLength(100)
+            .IsRequired(false);
+
+        builder.Property(e => e.ExternalId)
+            .HasMaxLength(100)
+            .IsRequired(false);
+
+        builder.Property(e => e.CreatedBy)
+            .HasMaxLength(EntitiesConstants.CreatedByMaxLength);
+
+        builder.Property(e => e.ModifiedBy)
+            .HasMaxLength(EntitiesConstants.ModifiedByMaxLength)
+            .IsRequired(false);
+
+        builder.HasOne(e => e.MeasurementPoint)
+            .WithMany()
+            .HasForeignKey(e => e.MeasurementPointId)
+            .OnDelete(DeleteBehavior.NoAction)
+            .IsRequired();
+
+        builder.HasOne(e => e.Product)
+            .WithMany()
+            .HasForeignKey(e => e.ProductId)
+            .OnDelete(DeleteBehavior.NoAction)
+            .IsRequired();
+
+        builder.HasIndex(e => e.MeasurementPointId);
+        builder.HasIndex(e => e.Number);
+        builder.HasIndex(e => e.FlowDirectionType);
+        builder.HasIndex(e => e.BeginTime);
+        builder.HasIndex(e => e.EndTime);
+        builder.HasIndex(e => e.ProductId);
+
+        builder.HasIndex(e => new { e.MeasurementPointId, e.FlowDirectionType, e.Number, e.BeginTime, e.EndTime })
+            .IsUnique();
+    }
+}
