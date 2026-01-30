1d69de2a6cb4a101a7434e243a0a0b1c9fca7e6e
kiryazovkosta <kosta.kiryazov@gmail.com>
2026-01-30 10:29:40 +0200

    Add Angular client to Aspire

--- ais-server/AccountabilityInformationSystem/AccountabilityInformationSystem.Aspire.AppHost/AppHost.cs
+++ ais-server/AccountabilityInformationSystem/AccountabilityInformationSystem.Aspire.AppHost/AppHost.cs
@@ -1,17 +1,27 @@
 using Aspire.Hosting;
+using Aspire.Hosting.JavaScript;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-var sqlServer = builder.AddSqlServer("ais-mssql")
+IResourceBuilder<SqlServerServerResource> sqlServer = builder.AddSqlServer("ais-mssql")
     .WithLifetime(ContainerLifetime.Persistent)
     .WithDataVolume("ais-mssqldata")
     .WithHostPort(11433);
 
-var database = sqlServer.AddDatabase("AccountabilityInformationSystemDb");
+IResourceBuilder<SqlServerDatabaseResource> database = sqlServer.AddDatabase("AccountabilityInformationSystemDb");
 
-builder.AddProject<Projects.AccountabilityInformationSystem_Api>("ais-api")
+IResourceBuilder<ProjectResource> backend = builder.AddProject<Projects.AccountabilityInformationSystem_Api>("ais-api")
     .WithReference(database)
     .WaitFor(sqlServer)
     .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development");
+
+var clientBuild = builder.AddExecutable(
+    "ais-client-build", "npm.cmd", workingDirectory: "../../../ais-client", "run", "build")
+    .WaitFor(backend);
+
+builder.AddExecutable(
+    "ais-client", "npm.cmd", workingDirectory: "../../../ais-client", "run", "start")
+    .WithEnvironment("BROWSER", "none")
+    .WaitFor(clientBuild);
 
 await builder.Build().RunAsync();
