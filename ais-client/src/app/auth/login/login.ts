ddc43851647526e3c1bff5fb5102bfe174bb3366
kiryazovkosta <kosta.kiryazov@gmail.com>
2026-03-01 15:12:42 +0200

    Add login as signal based form

--- ais-client/src/app/auth/login/login.ts
+++ ais-client/src/app/auth/login/login.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { form, FormField, required, email, submit } from '@angular/forms/signals'
+import { Router } from '@angular/router';
+import { LoginRequest } from './login.model';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  imports: [FormField],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Login {
-  username = '';
-  password = '';
+  private readonly router = inject(Router);
 
-  onSubmit() {
-    // TODO: implement authentication
+  loginRequest = signal<LoginRequest>({
+    email: '',
+    password: '',
+    remember: false
+  });
+
+  loginForm = form(this.loginRequest, (schemaPath) => {
+    required(schemaPath.email, { message: 'Email is required!'});
+    email(schemaPath.email, {message: 'Enter a valid email address'});
+
+    required(schemaPath.password, {message: 'Password is required'});
+  });
+
+  async onSubmit() {
+    await submit(this.loginForm, async () => {
+      const credentials = this.loginRequest();
+      console.log('Logging in with:', credentials);
+      this.router.navigate(['/home']);
+    });
   }
 }
